44587877ffbc968f846d4282a0dea820a9c44cf9
StefMa <StefMaDev@outlook.com>
2017-02-27 20:33:30 +0100

    Add some solutions

--- rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
+++ rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
@@ -1,45 +1,61 @@
 package org.sergiiz.rxkata;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.FutureTask;
-
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.FutureTask;
 
 class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Single<String> countryNameInCapitals(Country country) {
-        return null; // put your solution here
+        return Single.just(country.getName()).map(s -> s.toUpperCase(Locale.US));
     }
 
     public Single<Integer> countCountries(List<Country> countries) {
-        return null; // put your solution here
+        return Single.just(countries.size());
     }
 
     public Observable<Long> listPopulationOfEachCountry(List<Country> countries) {
-        return null; // put your solution here;
+        return Observable.fromIterable(countries).flatMap(country -> Observable.just(country.getPopulation()));
     }
 
     @Override
     public Observable<String> listNameOfEachCountry(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).flatMap(country -> Observable.just(country.getName()));
     }
 
     @Override
     public Observable<Country> listOnly3rdAnd4thCountry(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).filter(country -> {
+            int countryIndex = countries.indexOf(country);
+            return countryIndex == 2 || countryIndex == 3;
+        });
     }
 
+    // I'm not very happy with that solution :(
     @Override
     public Single<Boolean> isAllCountriesPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
+        return Single.just(countries).flatMap(countries1 -> {
+            for (int i = 0; i < countries1.size(); i++) {
+                if (countries1.get(i).getPopulation() < 1000000) {
+                    return Single.just(false);
+                }
+            }
+            return Single.just(true);
+        });
     }
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).filter(country -> country.getPopulation() >= 1000000);
     }
 
     @Override
@@ -49,7 +65,7 @@ class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).filter(country -> countryName.equals(country.getName())).map(Country::getCurrency).defaultIfEmpty("USD");
     }
 
     @Override
